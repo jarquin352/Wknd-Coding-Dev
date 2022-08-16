747e0c3176dc3b00b0990725142c961064ab251c
jarquin352 <62049935+jarquin352@users.noreply.github.com>
2022-08-16 15:33:47 -0700

    Update uci_web_scraper.py

--- uci_web_scraper.py
+++ uci_web_scraper.py
@@ -1,18 +1,43 @@
-
 import json
+import os.path #os.path.exists(path_to_file)
 import requests
+import re
 from bs4 import BeautifulSoup
 
 url = 'https://archive.ics.uci.edu/ml/datasets'
 
+def extract_contents(name_url):
+    result_lst = []
+    url = name_url
+    request = requests.get(url)
+    content = request.content
+    soup = BeautifulSoup(content, 'html.parser')
+    for i in soup.find_all("a",href = True):
+        #print(i['href'])
+        result_lst.append(i['href'])
+    return result_lst
 
+# def update_datasets():
 
-request = requests.get(url)
-content = request.content
-soup = BeautifulSoup(content, 'html.parser')
-for i in soup.find_all("a",href = True):
-    print(i['href'])
+############################################################################################################################################################
 
-URL1 = "https://archive.ics.uci.edu/ml/machine-learning-databases/abalone/abalone.names"
-response = requests.get(URL1)
-open("test.zip", "wb").write(response.content)
+add_to = ''
+input_url = 'https://archive.ics.uci.edu/ml/'
+lst_dt = []
+lst_dt = extract_contents(url)
+
+new_lst_dt = []
+
+for i in range(0,len(lst_dt)):
+    str_curr = re.search('datasets',lst_dt[i], re.I)
+    if str_curr == None or str_curr == 'datasets.php':
+        continue
+    new_lst_dt.append(input_url+lst_dt[i])
+
+#list to set to remove duplicates.
+new_lst_dt = list(set(new_lst_dt).copy())
+
+with open('dataset_links.txt','w') as f:
+    for i in new_lst_dt:
+        f.write(i)
+        f.write('\n')
