89b2768233109816e8e94018f977b746ab914286
jarquin352 <oscarjarquin352@outlook.com>
2022-08-23 12:09:43 -0700

    94% Complete in terms of functionality.

--- uci_web_scraper/uci_web_scraper.py
+++ uci_web_scraper/uci_web_scraper.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     ds_links_list = list(ds_links)
     string_ds_links = ''.join(ds_links_list)
     while True:
-        user_input = input('\nEnter a keyword for a data set (type  for \'all\' data set names, \'close\' to close the program, \'update\' to update your ML list): ')
+        user_input = input('\nEnter a keyword for a data set (type  for \'all\' data set names, \'close\' to close the program, \'update\' to update your ML list): ').strip()
         if user_input.casefold() == 'close':
             print("Goodbye!")
             break
@@ -23,6 +23,7 @@ if __name__ == "__main__":
         list_stored = []
         where_index = []
         #-------------------------------------------------------------------------
+        #searching for any keyword
         if user_input.casefold() != 'all':
             for i in range(len(ds_links_list)):
                 string_to_find = ds_links_list[i]
@@ -32,23 +33,39 @@ if __name__ == "__main__":
                     where_index.append(i)
                 else:
                     continue
+            if not list_stored:
+                print('No dataset found!')
+
+            for i in range(len(list_stored)):
+                test_url_w_contents = requests.get(list_stored[i]).content
+                soup_test = BeautifulSoup(test_url_w_contents,'html.parser')
+                url_header_text = soup_test.find_all(class_ = "heading")
+                for j in url_header_text:
+                    print(i,':',j.text)
+            
+            user_input = int(input('Select Data Set: '))
+            user_input = where_index[user_input]
+        
         else:
             for i in range(len(ds_links_list)):
                 word_to_print = ' '.join(re.findall(r'[A-Za-z]+',ds_links_list[i]))
                 print(i+1,':',word_to_print[38:])
+            user_input = int(input('Select Data Set: '))
+            user_input = user_input - 1 
 
         #-------------------------------------------------------------------------
         #second approach
-        for i in range(len(list_stored)):
-            test_url_w_contents = requests.get(list_stored[i]).content
-            soup_test = BeautifulSoup(test_url_w_contents,'html.parser')
-            url_header_text = soup_test.find_all(class_ = "heading")
-            for j in url_header_text:
-                print(j.text)
+        # for i in range(len(list_stored)):
+        #     test_url_w_contents = requests.get(list_stored[i]).content
+        #     soup_test = BeautifulSoup(test_url_w_contents,'html.parser')
+        #     url_header_text = soup_test.find_all(class_ = "heading")
+        #     for j in url_header_text:
+        #         print(i,':',j.text, '@ Index: ', where_index[i])
+
         #-------------------------------------------------------------------------
-        user_input = int(input('Select Data Set: '))
-        user_input = user_input - 1 
-        selected_url = ''
+        # user_input = int(input('Select Data Set: '))
+        # user_input = user_input - 1 
+        # selected_url = ''
 
         if ds_links_list[user_input] in ds_links:
             selected_url = global_url + ds_links[ds_links_list[user_input]]
